71f1e435d1f393b5ff90119ae43461da9c3240ca
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-04-16 16:02:41 +0200

    index bas

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <link href="styles/index.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+    <title>TITRE</title>
+  </head>
+
+<!-- <?php include "header.php";?> -->
+
+  <body>
+<center>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <br>
+
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <a href="#"><img src="images/aa_auto.png" width="350px" height="200px"></a>
+  <br><br>
+  <div class="bouton">
+  <p>
+    <a href="#">ALL »</a>
+  </p>
+</div><br><br>
+
+<div class="parallax-window" data-parallax="scroll" data-image-src="images/tips2.png" alt=""></div>
+
+
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <br>
+
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <a href="#"><img src="images/logosimplon.png" width="350px"></a>
+  <br><br>
+  <div class="bouton">
+  <p>
+    <a href="#">ALL »</a>
+  </p>
+</div>
+
+</center>
+
+    </body>
+
+<?php include "footer.php";?>
+<?php include "footer2.php";?>
+
+</html>
