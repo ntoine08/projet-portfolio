d7fb77c652e128892960f0e0799ab10ff00ada12
Valentin-Cordier <v.cordier@Simplon-Charleville.fr>
2019-04-16 16:51:43 +0200

    page projets

---
+++ projets.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="styles/tips.css" type="text/css" rel="stylesheet"/>
+</head>
+<html>
+<body>
+  <div class="header">
+    <?php include 'header.php' ?>
+  </div>
+  <div class="gallerie">
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/test.jpg" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    <span class="item">
+      <img src="images/tips.png" alt="à définir"/>
+    </span>
+    </div>
+</body>
+<footer>
+  <?php include 'footer.php' ?>
+  <?php include 'footer2.php' ?>
+</footer>
+</html>
