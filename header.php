44195cdab6d17d7b72187acac8cffd40038d1d2b
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-17 15:07:19 +0200

    nouveau header modifié de l'ancien

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <link href="styles/header.css" rel="stylesheet" type="text/css"/>
-<div class="grille1">
 <ul>
-  <div class="img">
-   <img src="images/logosimplon.png" height="75em" width="75em" alt="" />
-  </div>
-   <div class="nav">
-   <button href="#Acceuil" class="btn1">Acceuil</button>
-   <button href="#qui-je-suis" class="btn2">Qui Suis Je ?</button>
-   <button href="#parcours" class="btn3">Parcours</button>
-   <button href="#projets" class="btn4">Mes Projets</button>
-   <button href="#tips" class="btn5">Mes Tips .</button>
+   <div id="main">
+          <nav>
+            <img src="images/logosimplon.png" width="80px" height="80px;">
+               <ul>
+                    <li><a href="#">Acceuil</a></li>
+                    <li><a href="#">Qui Suis Je ?</a></li>
+                    <li><a href="#">Parcours</a></li>
+                    <li><a href="#">Mes Projets</a></li>
+                    <li><a href="#">Mes Tips</a></li>
+              </ul>
+         </nav>
    </div>
-</div>
 </ul>
