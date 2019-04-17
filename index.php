1f7ef82b464914a69d52711c1d26c8c31fa91a06
Linkels <m.chaumoitre@simplon-charleville.fr>
2019-04-18 01:23:23 +0200

    Commentaire HTML + CSS

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="styles/index.css" rel="stylesheet">
+    <!-- Pour le parallax -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+    <!-- Fin des liens pour parallax -->
     <title>TITRE</title>
   </head>
 
 <?php include "header.php";?>
   <body>
-		<!-- partie du slider-->
 
+        <!-- partie du slider-->
 		<div class="container">
 			<!--images du slide-->
 					<div class="mySlides">
@@ -47,11 +49,17 @@
 
     <script src="script.js"></script>
 		</div>
+    <!-- Fin du slider -->
 
 <br>
+
+<!-- parallax "Mes projets" -->
 <div class="parallax-projet" data-parallax="scroll" data-image-src="images/para.png" alt="">
   MES PROJETS</div>
+  <!-- fin de parallax "Mes projets" -->
+
 <center>
+  <!-- Début des 8 screens des projets accomplis -->
   <div id="proj">
   <a href="#"><img src="images/aa_auto.png" width="20%" height="200px"></a>
   <a href="#"><img src="images/aa_auto.png" width="20%" height="200px"></a>
@@ -69,11 +77,16 @@
     <a href="#">ALL »</a>
   </p>
 </div>
+<!-- Fin des screens des projets accomplis -->
+
 </div><br><br>
 
+<!-- Début parallax "Mes tips" -->
 <div class="parallax-tip" data-parallax="scroll" data-image-src="images/para.png" alt="">
 MES TIPS</div>
+<!-- Fin parallax "Mes tips" -->
 
+<!-- Début des 8 images des tips -->
 <div id="proj">
   <a href="#"><img src="images/logosimplon.png" width="350px"></a>
   <a href="#"><img src="images/logosimplon.png" width="350px"></a>
@@ -94,6 +107,7 @@ MES TIPS</div>
 </div>
 
 </center>
+<!-- Fin des tips -->
 
     </body>
 
