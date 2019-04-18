fbaa36ac6592a7b3e08522ea9508a479d347c117
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-18 13:43:37 +0200

    soucis footer régler, ajout du fichier metarialize.min.css dans le dossier styles

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" href="styles/materialize.min.css">
+<link href="styles/header.css" rel="stylesheet" type="text/css"/>
 <script src="js/header.js" charset="utf-8"></script>
 
 <!--Barre nav avec images-->
-<ul>
    <div id="main">
           <nav id="nav">
             <img src="images/blanc.png" width="80px" height="80px;">
@@ -15,4 +15,3 @@
               </ul>
          </nav>
    </div>
-</ul>
