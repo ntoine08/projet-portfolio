147624aa16f56cf2e36130a1f4effe5ada44b891
Chloé Servières <c.servieres@simplon-charleville.fr>
2019-04-18 14:13:33 +0200

    Pop up tips avec fond + cadre blanc

--- styles/tips.js
+++ styles/tips.js
@@ -1,4 +1,5 @@
 document.getElementById('affiche').addEventListener('click',
 function() {
   document.querySelector('.pop-tips').style.display = 'flex';
+  document.querySelector('.frame').style.display = 'flex';
 });
