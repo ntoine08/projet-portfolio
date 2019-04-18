83e86551ff9730f8082b682a6e4ee74891dd7a95
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-18 13:13:16 +0200

    ajout réel de Materialize Css

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
-https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css
-<link href="styles/header.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script src="js/header.js" charset="utf-8"></script>
 
 <!--Barre nav avec images-->
